b5629658a1a71ed59dd48d0a6b09f4f0d9a25774
SaraswatAditya <adityasaraswat92@gmail.com>
2024-07-29 12:42:50 +0530

    Vercel json added

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL;
+// const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = "http://pets-v2.dev-apis.com";
+
 
 const api = axios.create({
   baseURL: API_URL,
